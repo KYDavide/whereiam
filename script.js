02ee84c954a539644453db0ae699033e02a3147c
Davide <davide.brualdi@gmail.com>
2024-07-07 22:02:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     fetch('data.md')
         .then(response => response.text())
         .then(data => {
